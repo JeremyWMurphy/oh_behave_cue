9180c97091a809f8e39f2ef2a82611745613f04d
Jeremy Murphy <JeremyWMurphy@gmail.com>
2025-10-28 19:48:50 -0400

    cue and error sounds, teesny simplify

---
+++ oh_behave_cueS2/waveTable.h
@@ -0,0 +1,9 @@
+#ifndef _waveTable_h_
+#define _WaveTable_h_
+
+#define SamplesNum 20
+
+static uint16_t asymCos[SamplesNum] = 
+{0,221,715,1413,2207,2976,3604,3995,4095,4025,3820,3495,3071,2576,2046,1516,1022,597,272,0};
+
+#endif
